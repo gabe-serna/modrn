8cf784da4f7bc8bd663c28be5ff217c271f846da
Gabe Serna <gabriel.serna118@gmail.com>
2024-10-10 15:37:09 -0400

    Adjust Landing Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,18 +36,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
+              <nav className="w-full flex items-center space-x-8 justify-center border-b border-b-foreground/10 h-20 fixed bg-background z-50">
+                <Link
+                  href={"/decor"}
+                  className="uppercase text-lg text-gold-200 hover:text-gold-300 transition-colors"
+                >
+                  Decor
+                </Link>
+                <Link href={"/"} className="text-gold-500 text-4xl">
+                  MODRN
+                </Link>
+                <Link
+                  href={"/office"}
+                  className="uppercase text-lg text-gold-200 hover:text-gold-300 transition-colors"
+                >
+                  Office
+                </Link>
               </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">{children}</div>
+              <div className="flex flex-col gap-20 p-5 mt-24">{children}</div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import Image from "next/image";
+import HeroImage from "@/public/hero-image.png";
 
 export default async function Index() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <h1 className="text-6xl">Luxury Home Goods</h1>
+      <Image
+        src={HeroImage}
+        alt="Luxury Office Image"
+        className="opacity-40 w-full absolute top-0 left-0 z-0"
+      />
     </>
   );
 }
