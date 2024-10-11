902072c9f48000db9e4b47d0515e193a59dcd206
Gabe Serna <gabriel.serna118@gmail.com>
2024-10-11 15:51:30 -0400

    Fix: Refactor Changed function name

--- app/actions.ts
+++ app/actions.ts
@@ -1,21 +1,20 @@
 "use server";
 
 import { encodedRedirect } from "@/utils/utils";
-import { createClient } from "@/utils/supabase/server";
+import { supabaseAdmin } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const signUpAction = async (formData: FormData) => {
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
-  const supabase = createClient();
   const origin = headers().get("origin");
 
   if (!email || !password) {
     return { error: "Email and password are required" };
   }
 
-  const { error } = await supabase.auth.signUp({
+  const { error } = await supabaseAdmin.auth.signUp({
     email,
     password,
     options: {
@@ -38,9 +37,8 @@ export const signUpAction = async (formData: FormData) => {
 export const signInAction = async (formData: FormData) => {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
-  const supabase = createClient();
 
-  const { error } = await supabase.auth.signInWithPassword({
+  const { error } = await supabaseAdmin.auth.signInWithPassword({
     email,
     password,
   });
@@ -54,7 +52,6 @@ export const signInAction = async (formData: FormData) => {
 
 export const forgotPasswordAction = async (formData: FormData) => {
   const email = formData.get("email")?.toString();
-  const supabase = createClient();
   const origin = headers().get("origin");
   const callbackUrl = formData.get("callbackUrl")?.toString();
 
@@ -62,7 +59,7 @@ export const forgotPasswordAction = async (formData: FormData) => {
     return encodedRedirect("error", "/forgot-password", "Email is required");
   }
 
-  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+  const { error } = await supabaseAdmin.auth.resetPasswordForEmail(email, {
     redirectTo: `${origin}/auth/callback?redirect_to=/account/reset-password`,
   });
 
@@ -87,8 +84,6 @@ export const forgotPasswordAction = async (formData: FormData) => {
 };
 
 export const resetPasswordAction = async (formData: FormData) => {
-  const supabase = createClient();
-
   const password = formData.get("password") as string;
   const confirmPassword = formData.get("confirmPassword") as string;
 
@@ -108,7 +103,7 @@ export const resetPasswordAction = async (formData: FormData) => {
     );
   }
 
-  const { error } = await supabase.auth.updateUser({
+  const { error } = await supabaseAdmin.auth.updateUser({
     password: password,
   });
 
@@ -124,7 +119,6 @@ export const resetPasswordAction = async (formData: FormData) => {
 };
 
 export const signOutAction = async () => {
-  const supabase = createClient();
-  await supabase.auth.signOut();
+  await supabaseAdmin.auth.signOut();
   return redirect("/sign-in");
 };
