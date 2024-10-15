027ac3f3ce9980f0d2a3e8e179b399aaec026591
Gabe Serna <gabriel.serna118@gmail.com>
2024-10-15 13:35:30 -0400

    Style Admin Page a little bit

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,18 +1,9 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
-import { ShoppingCart, User } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
-
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import SignOutButton from "./SignOutButton";
+import AccountDropdown from "./AccountDropdown";
 
 const NavBar = async () => {
   const supabase = createClient();
@@ -54,48 +45,7 @@ const NavBar = async () => {
         <Link href="/cart">
           <ShoppingCart className="size-5 stroke-gold-200 transition-colors hover:stroke-gold-300" />
         </Link>
-        {user ? (
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger className="group">
-                  <User className="size-5 stroke-gold-200 transition-colors group-hover:stroke-gold-400" />
-                </NavigationMenuTrigger>
-                <NavigationMenuContent className="flex flex-col text-nowrap px-8 py-6">
-                  <h1 className="select-none text-lg font-bold">
-                    Your Account
-                  </h1>
-                  <SignOutButton />
-                  <hr className="mt-1.5 border-gold-800" />
-                  <Link href="/account" legacyBehavior passHref>
-                    <NavigationMenuLink className="mt-2 font-sans text-sm text-muted-foreground transition-colors hover:text-stone-500">
-                      Account
-                    </NavigationMenuLink>
-                  </Link>
-                  <Link href="/account/orders" legacyBehavior passHref>
-                    <NavigationMenuLink className="mt-0.5 font-sans text-sm text-muted-foreground transition-colors hover:text-stone-500">
-                      Orders
-                    </NavigationMenuLink>
-                  </Link>
-                  {isAdmin && (
-                    <>
-                      <hr className="mt-1.5 border-gold-800" />
-                      <Link href="/admin" legacyBehavior passHref>
-                        <NavigationMenuLink className="mt-0.5 font-sans text-sm text-stone-500 transition-colors hover:text-stone-600">
-                          Admin Panel
-                        </NavigationMenuLink>
-                      </Link>
-                    </>
-                  )}
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        ) : (
-          <Link href="/sign-in">
-            <User className="size-5 stroke-gold-200 transition-colors hover:stroke-gold-300" />
-          </Link>
-        )}
+        <AccountDropdown />
       </div>
     </nav>
   );
